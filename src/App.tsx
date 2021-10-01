c2842cd9ee427a059b1327438aa9a43ea89d9805
Steve Pae <stevepaemusic@gmail.com>
2021-10-01 15:21:43 -0400

    App.tsx
    - import MySlider
    -  add component to App

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import MyButton, {
   ButtonSize,
   ButtonType,
 } from "./components/MyButton/MyButton";
+import MySlider, { SliderSize } from "./components/MySlider/MySlider";
 
 function App() {
   return (
@@ -18,12 +19,54 @@ function App() {
       <MyButton buttonSize={ButtonSize.Small}>Small Button</MyButton>
       <MyButton buttonSize={ButtonSize.Large}>Large Button</MyButton>
       <h4>Types</h4>
-      <MyButton >Default Button</MyButton>
+      <MyButton>Default Button</MyButton>
       <MyButton buttonType={ButtonType.Primary}>Primary Button</MyButton>
       <MyButton buttonType={ButtonType.Secondary}>Secondary Button</MyButton>
       <MyButton buttonType={ButtonType.Danger}>Danger Button</MyButton>
-      <MyButton href={"https://www.google.com"}  buttonType={ButtonType.Link}>Link Button</MyButton>
-      <MyButton  href={"https://www.google.com"} buttonType={ButtonType.Link} disabled>Link Button</MyButton>
+      <MyButton href={"https://www.google.com"} buttonType={ButtonType.Link}>
+        Link Button
+      </MyButton>
+      <MyButton
+        href={"https://www.google.com"}
+        buttonType={ButtonType.Link}
+        disabled
+      >
+        Link Button
+      </MyButton>
+
+      <h2>Slider</h2>
+      <MySlider sliderSize={SliderSize.Large}>
+        <div className="slider-container">
+          <div className="slider-inner">
+            <div className="slider-item">
+              <img
+                src="https://source.unsplash.com/collection/190727/1600x900"
+                alt=""
+              />
+
+              <img
+                src="https://source.unsplash.com/collection/190728/1600x900"
+                alt=""
+              />
+
+              <img
+                src="https://source.unsplash.com/collection/190726/1600x900"
+                alt=""
+              />
+
+              <img
+                src="https://source.unsplash.com/collection/190725/1600x900"
+                alt=""
+              />
+
+              <img
+                src="https://source.unsplash.com/collection/190723/1600x900"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </MySlider>
     </div>
   );
 }
